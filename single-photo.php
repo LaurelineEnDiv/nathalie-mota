063f0c2807d13add1ae5eaccc5ce4271e9430b6b
Laureline Auger <laureline.auger@gmail.com>
2024-11-15 10:18:32 +0100

    affichage données single-photo.php

--- single-photo.php
+++ single-photo.php
@@ -9,19 +9,38 @@ if ( have_posts() ) :
         // Récupération des champs personnalisés
         $type = get_post_meta( get_the_ID(), 'type', true );  
         $ref = get_post_meta( get_the_ID(), 'ref', true );
+        // Récupération des taxonomies
+        $formats = get_the_terms( get_the_ID(), 'format' );  
+        $categories = get_the_terms( get_the_ID(), 'categorie' );  
+        // Récupération année (date de publication)
+        $annee = get_the_date('Y');
         ?>
         
         <figure id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <h1><?php the_title(); ?></h1>
+            <div class="photo-meta">
+                <p>Référence : <?php echo $ref; ?></p>
+                <p>Catégorie : 
+                    <?php
+                    foreach ($categories as $categorie) {
+                        echo $categorie->name . ' '; // Affiche le nom du terme
+                    }
+                    ?>
+                </p> 
+                <p>Format : 
+                    <?php
+                    foreach ($formats as $format) {
+                        echo $format->name . ' '; 
+                    }
+                    ?>
+                </p>
+                <p>Type : <?php echo $type; ?></p> 
+                <p>Année : <?php echo $annee; ?></p>
+            </div>
             <div class="content">
                 <?php the_content(); ?>
             </div>
-            <div class="photo-meta">
-                <p>Type :<?php echo $type; ?></p>
-                <p>Référence :<?php echo $ref; ?></p>
-            </div>
-
-    </figure>
+        </figure>
 
     <?php 
     endwhile; 
